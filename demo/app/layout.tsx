ca61f6c0c5bce78f9d6a89ad7be476fdfdd53672
kirilinsky <kirill.ilinsky@gmail.com>
2026-03-24 00:56:02 +0100

    feat: add vercel analytics

--- demo/app/layout.tsx
+++ demo/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Analytics />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
