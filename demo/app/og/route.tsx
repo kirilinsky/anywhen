2994a2fdd50f8714970de0eefd5291aaa142c9e5
kirilinsky <kirill.ilinsky@gmail.com>
2026-03-25 07:03:16 +0100

    fix: og generator

--- demo/app/og/route.tsx
+++ demo/app/og/route.tsx
@@ -4,7 +4,7 @@ export const runtime = "edge";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
-export default function OG() {
+export function GET() {
   return new ImageResponse(
     <div
       style={{
